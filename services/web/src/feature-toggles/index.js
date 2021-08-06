1ee255c3dfd2a574f78b6996937e3ccb7aa63306
bdushimi <lespharaons@hotmail.com>
2021-08-06 19:15:48 +0200

    Adds feature toggles configs

--- services/web/src/feature-toggles/index.js
+++ services/web/src/feature-toggles/index.js
@@ -1,3 +1,7 @@
+import { featureToggleStore} from "./store"
+
+
+
 export const FEATURE_TOGGLES = {
     vipWelcome: {
       name: "feature_vip_welcome"
@@ -5,9 +9,20 @@ export const FEATURE_TOGGLES = {
   };
 
 
+  
+const setFeatureTogglesStore = (configValues) => 
+Object.keys(FEATURE_TOGGLES).forEach((toggleKey) => {
+  const featureToggleName = FEATURE_TOGGLES[toggleKey].name;
+  if (configValues[featureToggleName]) {
+    featureTogglesStore.setFeatureToggle(featureToggleName,
+      configValues[featureToggleName].asBoolean());
+  }
+})
+
 
   export const initFeatureToggles = async () => {
     const remoteConfig = window.firebase.remoteConfig();
     await remoteConfig.fetchAndActivate();
+    setFeatureTogglesStore(remoteConfig.getAll())
     return remoteConfig;
   };
---
+++ services/web/src/feature-toggles/store.js
@@ -0,0 +1,18 @@
+import { writable} from 'svelte/store';
+
+const createStore = () => {
+
+    const { subscribe, update } = writable({});
+
+    return {
+        subscribe,
+        setFeatureToggle: (featureToggleName, isEnabled) => 
+        update(store => ({
+            ...store,
+            [featureToggleName]:isEnabled
+        }))
+    }
+}   
+
+
+export const featureToggleStore = createStore()
