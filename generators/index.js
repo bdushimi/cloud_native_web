ca7f990bb78dafa983b85dedf613a30c9d055522
DUSHIMIMANA Bernard <35345818+bdushimi@users.noreply.github.com>
2021-08-12 08:43:04 +0200

    Adds a trivial comment

--- generators/index.js
+++ generators/index.js
@@ -1,6 +1,7 @@
 const gitConfigHelper = require("plop-helper-git-config");
 const serviceGenerator = require("./service");
 
+// testing this Github web editor
 module.exports = function (plop) {
   plop.setHelper("gitConfig", gitConfigHelper);
   plop.setGenerator("service", serviceGenerator(plop));
